c5815bbbf19134bddc534f37b5354decb7f6ef45
rssson <154929605+rssson@users.noreply.github.com>
2024-10-21 14:53:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1923,14 +1923,7 @@ Game.Launch=function()
 		else Game.Loader.loaded=callback;
 		Game.Loader.Load(['filler.png']);
 	}
-	Game.ErrorFrame=function()
-	{
-		l('offGameMessage').innerHTML=
-		'<div class="title">Oops. Wrong address!</div>'+
-		'<div>It looks like you\'re accessing Cookie Clicker from another URL than the official one.<br>'+
-		'You can <a href="//orteil.dashnet.org/cookieclicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
-		'<small>(If for any reason, you are unable to access the game on the official URL, we are currently working on a second domain.)</small></div>';
-	}
+	
 	Game.timedout=false;
 	Game.Timeout=function()
 	{
@@ -1966,7 +1959,7 @@ Game.Launch=function()
 		Game.T=0;
 		Game.drawT=0;
 		Game.loopT=0;
-		Game.fps=30;
+		Game.fps=1000;
 		
 		Game.season=Game.baseSeason;
 		
